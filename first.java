7c60775e5a5716b2289ceec5b7acedea8227cfdf
MXQ8IUN <mark_x_qi@homedepot.com>
2018-07-03 13:42:26 -0400

    syncing java program + gitignore

---
+++ first.java
@@ -0,0 +1,10 @@
+class first{
+    public static void main(String[] args) {
+        String hi = "hi buddy! ";
+        int[] twelve = {1,2,3};
+        System.out.println("Hello World");
+        System.out.print(hi);
+        System.out.print(hi);
+        System.out.println(twelve[0]);
+    }
+}
