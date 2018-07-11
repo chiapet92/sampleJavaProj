e36effb4455fbc0291277a6746e65c9fdfd40e31
MXQ8IUN <mark_x_qi@homedepot.com>
2018-07-11 13:15:54 -0400

    committing some java examples for enum implementation

---
+++ enum_examples/enum+instance/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+		//whois.arin.net
+        System.out.println(WhoisRIR.ARIN.url());
+
+    }
+
+}
---
+++ enum_examples/enum+method+operation/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        double result = Operation.PLUS.calculate(1, 2);
+        System.out.println(result); //3.0
+
+    }
+
+}
